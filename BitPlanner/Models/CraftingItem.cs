455c6ce77bf9e0e3876adab10abb8d8ef9a4186b
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2025-07-02 01:06:25 +0300

    Fix getting generic name of item

--- BitPlanner/Models/CraftingItem.cs
+++ BitPlanner/Models/CraftingItem.cs
@@ -10,5 +10,23 @@ public class CraftingItem
     public List<Recipe> Recipes { get; set; } = [];
     public int ExtractionSkill { get; set; } = -1;
     public bool Craftable => Recipes.Count > 0;
-    public string GenericName => Name.Substring(Math.Max(0, Name.IndexOf(' ') + 1));
+    public string GenericName
+    {
+        get
+        {
+            foreach (var prefix in new string[]{
+                "Rough", "Simple", "Sturdy", "Fine", "Exquisite", "Peerless", "Ornate", "Pristine", "Magnificent", "Flawless",
+                "Ferralith", "Pyrelite", "Emarium", "Elenvar", "Luminite", "Rathium", "Aurumite", "Celestium", "Umbracite", "Astralite",
+                "Automata's", "Basic", "Infused", "Magnificient"
+            })
+            {
+                var index = Name.IndexOf(prefix);
+                if (index > -1)
+                {
+                    return Name.Remove(index, prefix.Length + 1);
+                }
+            }
+            return Name;
+        }
+    }
 }
